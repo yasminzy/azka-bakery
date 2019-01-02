95d8b5009a13a92f1d7315a827184af52134bba9
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2019-01-02 18:46:01 +0800

    Edit formatting

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,16 +50,16 @@ module.exports = {
     extend(config, ctx) {
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
-        })
+        });
       }
-    },
+    }
   },
   router: {
-    scrollBehavior: function(to, from, savedPosition) {
+    scrollBehavior: function() {
       return { x: 0, y: 0 };
     }
   }
