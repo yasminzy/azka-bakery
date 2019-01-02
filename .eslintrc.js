70b8d5f5bbb6eb41a227ba64bf0779fefd47b60a
Yasmin Zulfati Yusrina <yzyusrina@ymail.com>
2019-01-02 14:34:43 +0800

    Turn off some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,14 @@ module.exports = {
     parser: "babel-eslint"
   },
   extends: ["plugin:vue/recommended", "plugin:prettier/recommended"],
-  plugins: ["vue"],
+  plugins: ["vue", "prettier"],
   rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/component-name-in-template-casing": "off",
+    "vue/html-self-closing": "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/singleline-html-element-content-newline": "off",
     "vue/v-bind-style": "off"
   }
 };
